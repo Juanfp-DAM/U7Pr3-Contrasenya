c39ddb8c5559c2feb349030d607e336ec0c21a15
dam_fpj <juaferpui@alu.edu.gva.es>
2024-05-07 13:23:27 +0200

    Creación del método y se pasan los dos primeros tests correctamente.

--- src/Contrasenya/PasswordVerify.java
+++ src/Contrasenya/PasswordVerify.java
@@ -3,5 +3,66 @@ package Contrasenya;
 public class PasswordVerify {
 
 
+    private String pass;
+    private boolean verificaPass;
+
+    public PasswordVerify() {
+
+    }
+
+    public String passwordString(String psw){
+        String requisitos = "";
+        int tamanyoPalabras;
+        if(psw==null){
+            requisitos += "La contraseña ha de tener almenos 8 carácteres. ";
+            tamanyoPalabras=0;
+        } else if(psw.length()<8){
+            requisitos += "La contraseña ha de tener almenos 8 carácteres. ";
+            tamanyoPalabras=psw.length();
+        }else{
+            tamanyoPalabras=psw.length();
+        }
+
+        //Verificación que contenga 2 números o más
+        int cuentaNumeros =0;
+        int cuentaMayus = 0;
+        int cuentaSimbolo=0;
+
+        for (int i = 0; i < tamanyoPalabras; i++) {
+            String guardaLetra = String.valueOf(psw.charAt(i));
+            if(guardaLetra.matches("[0-9]")){
+                cuentaNumeros++;
+            }
+            if(guardaLetra.matches("[A-Z]")){
+                cuentaMayus++;
+            }
+            //Si tiene cualquier cosa distinta de A-Z, a-z, 0-9
+            if(guardaLetra.matches("[^\\w\\s]")){
+                cuentaSimbolo++;
+            }
+        }
+        if(cuentaNumeros<2){
+            requisitos+="La contraseña ha de tener 2 dígitos o más. ";
+        }
+        if(cuentaMayus==0){
+            requisitos+="La contraseña ha de tener al menos una letra mayúscula. ";
+        }
+        if(cuentaSimbolo==0){
+            requisitos+="La contraseña ha de tener al menos un carácter especial.";
+        }
+
+        return requisitos;
+    }
+
+    public boolean verificaPassw(){
+        boolean verificador = true;
+        if(!passwordString(pass).equals("")){
+            verificador=false;
+        }
+        return verificador;
+    }
+
+
+
 
 }
--- src/Contrasenya/PasswordVerifyTest.java
+++ src/Contrasenya/PasswordVerifyTest.java
@@ -1,7 +1,46 @@
 package Contrasenya;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordVerifyTest {
+    static String pocasLetras = "La contraseña ha de tener almenos 8 carácteres. ";
+    static String pocosNumeros =  "La contraseña ha de tener 2 dígitos o más. ";
+    static String pocasMayus = "La contraseña ha de tener al menos una letra mayúscula. ";
+    static String pocosCaracteresEspeciales = "La contraseña ha de tener al menos un carácter especial.";
+    static PasswordVerify p;
+
+    @Test
+    void passVacia(){
+        //ARRANGE
+        p = new PasswordVerify();
+        String contrasenya = "";
+
+        //ACT
+        String resultado = p.passwordString(contrasenya);
+        boolean verifica = p.verificaPassw();
+
+        //ASSERTEQUALS
+        assertEquals(pocasLetras + pocosNumeros + pocasMayus + pocosCaracteresEspeciales ,resultado);
+        assertFalse(verifica);
+    }
+
+    @Test
+    void passhola(){
+        //ARRANGE
+        p = new PasswordVerify();
+        String contrasenya = "hola";
+
+        //ACT
+        String resultado = p.passwordString(contrasenya);
+        boolean verifica = p.verificaPassw();
+
+        //ASSERTEQUALS
+        assertEquals(pocasLetras + pocosNumeros + pocasMayus + pocosCaracteresEspeciales ,resultado);
+        assertFalse(verifica);
+    }
+
+
 
 }
